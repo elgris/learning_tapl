6f1822a32b92e3f64cffb009c2fe7c08b9f91621
elgris <no@mail.me>
2025-06-21 12:51:18 +0200

    Instantiate and run the parser on test input.

--- kotlin/arith/src/main/kotlin/Main.kt
+++ kotlin/arith/src/main/kotlin/Main.kt
@@ -1,7 +1,15 @@
 package arith
 
-import arith.parser.Parser;
+import arith.parser.Parser.parse;
+import java.io.StringReader
+
+val input = """
+0;
+succ (pred 0);
+iszero (pred (succ (succ 0)));    
+""".trimIndent()
+
 fun main() {
-    val parser = Parser(System.`in`);
-    val ast = parser.Parse();
+    val ast = parse(input.byteInputStream())
+    println(ast)
 }
