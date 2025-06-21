4265790cf7f6217e9a813f0042dbe9989b649d59
elgris <no@mail.me>
2025-06-21 13:00:12 +0200

    Makes input constant processable and adds TODOs.

--- kotlin/arith/src/main/kotlin/Main.kt
+++ kotlin/arith/src/main/kotlin/Main.kt
@@ -3,12 +3,11 @@ package arith
 import arith.parser.Parser.parse;
 import java.io.StringReader
 
-val input = """
-0;
-succ (pred 0);
-iszero (pred (succ (succ 0)));    
-""".trimIndent()
+const val input = "if false then true else false"
 
+// TODO: support semicolon as a separator
+// TODO: support parenthesis
+// TODO: add evaluator
 fun main() {
     val ast = parse(input.byteInputStream())
     println(ast)
